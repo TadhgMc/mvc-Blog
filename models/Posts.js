7dbaaffa0c499e893d8cf6b56eb3fb6980bceb40
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-04-27 09:42:14 -0400

    made models and index for db

---
+++ models/Posts.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Posts extends Model {}
+
+Posts.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_body: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+            isEmail: true,
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts',
+    }
+);
+
+module.exports = Posts;
