436901ce4099733e2c81fdd49d6526a09650c98b
Tadhg McIntyre <mcintyretadhg@gmail.com>
2021-04-30 14:02:20 -0400

    various bug fixes

--- models/index.js
+++ models/index.js
@@ -2,22 +2,38 @@ const User = require('./User');
 const Posts = require('./Posts');
 const Comments = require('./Comments');
 
+//1 to many
 User.hasMany(Posts, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
 
-Posts.belongsTo(User, {
-    foreignKey: 'user_id',
-});
-
+//1 to many
 User.hasMany(Comments, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
 
+//1 to 1
+Posts.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+//1 to many
+Posts.hasMany(Comments, {
+    foreignKey: 'post_id',
+});
+
+//1 to 1
 Comments.belongsTo(User, {
     foreignKey: 'user_id',
-})
+});
+
+//1 to 1
+Comments.belongsTo(Posts, {
+    foreignKey: 'post_id',
+});
+
+
 
 module.exports = { User, Posts, Comments, };
